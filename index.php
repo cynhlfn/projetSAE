2004ac101819b604158c1b0d977453a484ae34fa
Cyndia HALFOUN <halfoun.c@gmail.com>
2026-06-17 21:25:57 +0200

    feature: squelette html du fichier index.php

--- index.php
+++ index.php
@@ -1,3 +1,40 @@
 <?php
-// public/index.php
-echo "Projet SAE - En construction";
+require 'config/database.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Wikifilm</title>
+  <!-- bootstrap css -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+  <!-- notre css -->
+  <link rel="stylesheet" href="public/css/style.css">
+</head>
+
+<body>
+  <!-- header pour : logo + genres -->
+  <header>
+
+  </header>
+
+  <!-- recherche + tri -->
+  <section id="search-sort">
+
+  </section>
+
+  <!-- afichage principale de la liste des filmes -->
+  <main>
+    <!--cartes des filmes  -->
+  </main>
+
+  <!-- bootstrap javascript  -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+  <!-- notre javascript -->
+  <script src="public/js/main.js"></script>
+</body>
+
+</html>
